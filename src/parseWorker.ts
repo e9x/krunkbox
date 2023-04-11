097cde8d7131fda5160efea036485235ae162378
David Reed <reedswork@proton.me>
2023-04-11 14:36:31 -0400

    Use @tdewolff/minify instead of terser
    much faster

--- src/parseWorker.ts
+++ src/parseWorker.ts
@@ -1,14 +1,14 @@
+import { string } from "@tdewolff/minify";
 import { writeFile } from "fs/promises";
-import { minify } from "terser";
 
 export async function parse(gameScript: string) {
   await writeFile(new URL("../bin/game.debug.js", import.meta.url), gameScript);
 
-  console.log("Parsing game...");
+  console.log("Minifying game...");
 
-  console.time("Parse");
-  const minified = (await minify(gameScript)).code!;
-  console.timeEnd("Parse");
+  console.time("Minify");
+  const minified = string("application/javascript", gameScript);
+  console.timeEnd("Minify");
 
   await writeFile(new URL("../bin/game.min.js", import.meta.url), minified);
 }
