89ba337b164617c0f8be4f56c57d894cdb9d7cab
David Reed <reedswork@proton.me>
2023-04-12 15:47:38 -0400

    deobfuscate

--- src/parseWorker.ts
+++ src/parseWorker.ts
@@ -1,14 +1,25 @@
 import { binDir } from "./updateBin.js";
 import { string } from "@tdewolff/minify";
+import { Deobfuscator } from "deobfuscator";
 import { writeFile } from "node:fs/promises";
 
-export async function parse(gameScript: string) {
+export default async function parseGame(gameScript: string) {
   await writeFile(new URL("./game.debug.js", binDir), gameScript);
 
   console.log("Minifying game...");
 
+  console.time("Deobfuscate");
+
+  // crack(gameScript):                                       27.367s
+  // await new Deobfuscator().deobfuscateSource(gameScript):  8:38.864 (m:ss.mmm) fail
+  // ^ but only StringDecoder:                                35.158s
+  const deobfuscated = await new Deobfuscator().deobfuscateSource(gameScript, {
+    customTransformers: [["StringDecoder", []]],
+  });
+  console.timeEnd("Deobfuscate");
+
   console.time("Minify");
-  const minified = string("application/javascript", gameScript);
+  const minified = string("application/javascript", deobfuscated);
   console.timeEnd("Minify");
 
   await writeFile(new URL("./game.min.js", binDir), minified);
