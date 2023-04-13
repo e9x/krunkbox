3396f898c4d884143fc74a0e4a84cb0451d966bb
David Reed <reedswork@proton.me>
2023-04-13 16:03:16 -0400

    convert into userscript

--- sploit.user.js
+++ sploit.user.js
@@ -1,4 +1,93 @@
-(async () => {
+// ==UserScript==
+// @name         Sploit
+// @namespace    https://sys32.dev/
+// @version      2.0.0-alpha
+// @description  try to take over the world!
+// @author       You
+// @match        https://krunker.io/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=krunker.io
+// @grant        GM_openInTab
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_xmlhttpRequest
+// @connect      imsoware.sys32.dev
+// @connect      [::1]
+// @run-at       document-start
+// ==/UserScript==
+
+/* eslint-env es6 */
+/* eslint: eslint:recommended */
+/* globals GM_xmlhttpRequest, GM_deleteValue, GM_getValue, GM_setValue, GM_openInTab */
+
+// TODO: use GM storage instead of localStorage
+(() => {
+  "use strict";
+
+  async function main() {
+    /**
+     * @type {API|undefined}
+     */
+    let api;
+
+    while (!api) {
+      const apiToken = await getToken(workInkURL);
+
+      if (!apiToken) return; // aborted
+
+      try {
+        api = new API(apiToken);
+        if (!(await api.valid())) api = undefined;
+      } catch (err) {
+        // server error, try again in some
+        console.log("Server error, trying again in 3s");
+        await sleep(3e3);
+      }
+    }
+
+    const token = await (
+      await fetch("https://matchmaker.krunker.io/generate-token")
+    ).arrayBuffer();
+
+    /**
+     * @type {string|undefined}
+     */
+    let hashed;
+
+    while (!hashed) {
+      const gotHashed = await api.hash(token);
+
+      // this shouldn't happen
+      if (gotHashed === APIError.BadToken) throw new Error("Bad token!");
+
+      if (gotHashed === APIError.Early) {
+        console.log("Too early, trying again in 3s");
+        await sleep(3e3);
+      }
+
+      hashed = gotHashed;
+    }
+
+    const seekGame = await fetch(
+      `https://matchmaker.krunker.io/seek-game?${new URLSearchParams({
+        hostname: "krunker.io",
+        region: "de-fra",
+        autoChangeGame: "false",
+        validationToken: hashed
+          .split("")
+          .map((c) => String.fromCharCode(c.charCodeAt(0) - 10))
+          .join(""),
+        dataQuery: JSON.stringify({ v: "5pliwbJakrvPpOnR5pA5V7dPpSeq8f28" }),
+      })}`
+    );
+
+    console.log(
+      "Response from seek-game:",
+      seekGame.status,
+      await seekGame.json().catch(() => Symbol("INVALID JSON"))
+    );
+  }
+
   const apiURL = "http://[::1]:3001/";
   const workInkURL = "https://work.ink/4lH/krunker";
 
@@ -20,23 +109,75 @@
     Early: 1,
   };
 
+  /**
+   * @typedef {Object} GMFetchOptions
+   * @property {string} [method]
+   * @property {string} [body]
+   * @property {Record<string, string>} [headers]
+   */
+
+  /**
+   * @typedef {Object} GMFetchResponse
+   * @property {number} status
+   * @property {string} statusText
+   * @property {boolean} ok
+   * @property {Headers} headers
+   * @property {() => Promise<text>} text
+   */
+
+  /**
+   *
+   * @param {string} url
+   * @param {GMFetchOptions} [opts]
+   * @returns {Promise<GMFetchResponse>}
+   */
+  function gmFetch(url, opts) {
+    return new Promise((resolve, reject) =>
+      GM_xmlhttpRequest({
+        url,
+        method: opts.method,
+        data: opts.body,
+        headers: opts.headers,
+        onerror: (e) => reject(e.error),
+        onload: (res) =>
+          resolve({
+            status: res.status,
+            statusText: res.statusText,
+            ok: res.status >= 200 && res.status < 300,
+            text: () => Promise.resolve(res.responseText),
+            headers: res.responseHeaders
+              .split("\r\n")
+              .filter(Boolean) // empty lines
+              .reduce((headers, line) => {
+                const [name, ...value] = line.split(": ");
+                headers.set(name, value.join(": "));
+                return headers;
+              }, new Headers()),
+          }),
+      })
+    );
+  }
+
   class API {
     /**
      * @type {string|undefined}
      */
     #token;
+
     /**
      *
      * @param {string} token
      */
     static async processWorkInk(token) {
-      const res = await fetch(new URL("hi", apiURL), {
+      const res = await gmFetch(new URL("hi", apiURL).toString(), {
         method: "POST",
         body: token,
         headers: {
           "content-type": "text/plain",
         },
       });
+
+      console.log(res);
 
       if (res.status === 402) return WorkInkErrors.BadToken;
       if (res.status === 422) return WorkInkErrors.DuplicateToken;
@@ -58,8 +199,8 @@
     }
     set token(value) {
       this.#token = value;
-      if (value === undefined) delete localStorage.sample_token;
-      else localStorage.sample_token = value;
+      if (value === undefined) GM_deleteValue("token");
+      else GM_setValue("token", value);
     }
     /**
      *
@@ -67,7 +208,7 @@
      * @param {string} token
      */
     async hash(token) {
-      const res = await fetch(new URL("hash", apiURL), {
+      const res = await gmFetch(new URL("hash", apiURL).toString(), {
         method: "POST",
         body: token,
         headers: {
@@ -91,7 +232,7 @@
      * Validates the token. Should be called before making any requests to Krunker's matchmaker
      */
     async valid() {
-      const res = await fetch(new URL("me", apiURL), {
+      const res = await gmFetch(new URL("me", apiURL), {
         method: "POST",
         body: this.token,
         headers: {
@@ -120,10 +261,11 @@
     /**
      * @type {string|undefined}
      */
-    let token = localStorage.sample_token;
+    let token = GM_getValue("token");
 
     while (!token) {
-      window.open(workInk, "_blank");
+      GM_openInTab(workInk);
+      console.trace("Prompt for access key...");
       const key = prompt("Enter access key here");
       // cancel
       if (key === undefined) return;
@@ -131,7 +273,10 @@
       if (res === WorkInkErrors.BadToken) alert("Bad token. Try again.");
       else if (res === WorkInkErrors.DuplicateToken)
         alert("Token already used. Try again.");
-      else token = res;
+      else {
+        console.log("Got token:", res);
+        token = res;
+      }
     }
 
     return token;
@@ -146,62 +291,5 @@
     return new Promise((resolve) => setTimeout(() => resolve(), ms));
   }
 
-  /**
-   * @type {API|undefined}
-   */
-  let api;
-
-  while (!api)
-    try {
-      api = new API(await getToken(workInkURL));
-      if (!(await api.valid())) api = undefined;
-    } catch (err) {
-      // server error, try again in some
-      console.log("Server error, trying again in 3s");
-      await sleep(3e3);
-    }
-
-  const token = await (
-    await fetch("https://matchmaker.krunker.io/generate-token")
-  ).arrayBuffer();
-
-  /**
-   * @type {string|undefined}
-   */
-  let hashed;
-
-  while (!hashed) {
-    const gotHashed = await api.hash(token);
-
-    // this shouldn't happen
-    if (gotHashed === APIError.BadToken) throw new Error("Bad token!");
-
-    if (gotHashed === APIError.Early) {
-      console.log("Too early, trying again in 3s");
-      await sleep(3e3);
-    }
-
-    hashed = gotHashed;
-  }
-
-  const seekGame = await fetch(
-    `https://matchmaker.krunker.io/seek-game?${new URLSearchParams({
-      hostname: "krunker.io",
-      region: "us-nj",
-      autoChangeGame: "false",
-      validationToken: hashed
-        .split("")
-        .map((argInstantPlease) =>
-          String.fromCharCode(argInstantPlease.charCodeAt(0) - 10)
-        )
-        .join(""),
-      dataQuery: JSON.stringify({ v: "dqk8nbmX7Juu0f4b62wtlwM6pw8ytLHG" }),
-    })}`
-  );
-
-  console.log(
-    "Response from seek-game:",
-    seekGame.status,
-    await seekGame.json().catch(() => Symbol("INVALID JSON"))
-  );
+  main();
 })();
