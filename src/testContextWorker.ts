ffb1a464953ad4ff4b28d2194a25b44fa81eb390
David Reed <reedswork@proton.me>
2023-04-16 22:03:08 -0400

    remove testParse, update ctx test

--- src/testContextWorker.ts
+++ src/testContextWorker.ts
@@ -3,4 +3,4 @@ import { game } from "./contextWorker.js";
 
 const src = await game();
 
-console.log(typeof src === "string" ? src.slice(0, 1000) : "Fail");
+console.log(src);
--- src/testParse.ts
+++
@@ -1,6 +0,0 @@
-import "source-map-support/register.js";
-import parseGame from "./parseWorker.js";
-import { binDir } from "./updateBin.js";
-import { readFile } from "node:fs/promises";
-
-await parseGame(await readFile(new URL("./game.debug.js", binDir), "utf-8"));
