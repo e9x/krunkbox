ffc8d76d6db909bf39aa85b7eb9d9120e10e9cb7
David Reed <reedswork@proton.me>
2023-05-13 13:32:11 -0400

    fix logic

--- src/purgeTokens.ts
+++ src/purgeTokens.ts
@@ -12,7 +12,7 @@ export async function tokenShouldPurge(token: string) {
   const {
     rows: [found],
   } = await db.query(
-    "SELECT * FROM token_data WHERE current_token = $1 AND lifetime OR NOT (uses > 40 OR created_at + INTERVAL '1 days' > NOW());",
+    "SELECT * FROM token_data WHERE current_token = $1 AND (lifetime OR (uses <= 40 AND created_at + INTERVAL '1 days' > NOW()));",
     [token]
   );
 
