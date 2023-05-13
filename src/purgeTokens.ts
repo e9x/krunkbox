c7d7ad2c87eaa0cce2476e69a93a8af120481291
David Reed <reedswork@proton.me>
2023-05-13 13:32:20 -0400

    45 uses

--- src/purgeTokens.ts
+++ src/purgeTokens.ts
@@ -3,7 +3,7 @@ import db from "./db";
 export async function purgeTokens() {
   return (
     await db.query(
-      "DELETE FROM token_data WHERE NOT lifetime AND (uses > 40 OR created_at + INTERVAL '1 days' > NOW());"
+      "DELETE FROM token_data WHERE NOT lifetime AND (uses > 45 OR created_at + INTERVAL '1 days' > NOW());"
     )
   ).rowCount;
 }
@@ -12,7 +12,7 @@ export async function tokenShouldPurge(token: string) {
   const {
     rows: [found],
   } = await db.query(
-    "SELECT * FROM token_data WHERE current_token = $1 AND (lifetime OR (uses <= 40 AND created_at + INTERVAL '1 days' > NOW()));",
+    "SELECT * FROM token_data WHERE current_token = $1 AND (lifetime OR (uses <= 45 AND created_at + INTERVAL '1 days' > NOW()));",
     [token]
   );
 
