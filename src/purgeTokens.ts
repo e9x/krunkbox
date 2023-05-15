dfc4852bfd720f9e2afbdee51dec169161cf6a59
David Reed <reedswork@proton.me>
2023-05-15 19:27:17 -0400

    increase interval to 2 days and 56 uses
    this is so we don't spam users with linkvertise
    and get banned

--- src/purgeTokens.ts
+++ src/purgeTokens.ts
@@ -3,7 +3,7 @@ import db from "./db";
 export async function purgeTokens() {
   return (
     await db.query(
-      "DELETE FROM lv_token_data WHERE NOT lifetime AND (uses > 30 OR created_at + INTERVAL '1 days' > NOW());"
+      "DELETE FROM lv_token_data WHERE NOT lifetime AND (uses > 56 OR created_at + INTERVAL '2 days' > NOW());"
     )
   ).rowCount;
 }
@@ -12,7 +12,7 @@ export async function tokenShouldPurge(token: string) {
   const {
     rows: [found],
   } = await db.query(
-    "SELECT * FROM lv_token_data WHERE current_token = $1 AND (lifetime OR (uses <= 30 AND created_at + INTERVAL '1 days' > NOW()));",
+    "SELECT * FROM lv_token_data WHERE current_token = $1 AND (lifetime OR (uses <= 56 AND created_at + INTERVAL '2 days' > NOW()));",
     [token]
   );
 
