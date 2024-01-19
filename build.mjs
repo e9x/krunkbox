c0b47030b85caef3df6555682a0611bd96fe3fac
David Reed <reedswork@proton.me>
2024-01-19 06:52:18 -0500

    esbuild

---
+++ build.mjs
@@ -0,0 +1,37 @@
+import { build } from "esbuild";
+
+await build({
+  platform: "node",
+  entryPoints: ["src/server.ts"],
+  outfile: "dist/server.js",
+  format: "esm",
+  sourcemap: true,
+  bundle: true,
+  minify: true,
+  packages: "external",
+  tsconfig: "src/tsconfig.json",
+});
+
+await build({
+  platform: "node",
+  entryPoints: ["src/parseWorker.ts"],
+  outfile: "dist/parseWorker.js",
+  format: "esm",
+  sourcemap: true,
+  bundle: true,
+  minify: true,
+  packages: "external",
+  tsconfig: "src/tsconfig.json",
+});
+
+await build({
+  platform: "node",
+  entryPoints: ["client/preload.ts"],
+  outfile: "dist/preload.js",
+  format: "cjs",
+  sourcemap: "inline",
+  bundle: true,
+  minify: true,
+  packages: "external",
+  tsconfig: "src/tsconfig.json",
+});
