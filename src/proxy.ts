2a07ddd4a10b37cca3fb9f0c807f745753995789
David Reed <reedswork@proton.me>
2025-05-11 20:56:06 +0000

    use mullvad proxies

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,16 +1,21 @@
 import { SocksProxyAgent } from "socks-proxy-agent";
 import { HttpsProxyAgent } from 'https-proxy-agent';
 
-function pickProxy() {
-    const PROXY = process.env["PROXY"]?.split(",");
-    if (PROXY === undefined) {
-        console.error("pls set PROXY g")
-        process.exit(1)
-    }
-    return PROXY[~~(Math.random() * PROXY.length)]
+async function pickProxy() {
+    // const PROXY = process.env["PROXY"]?.split(",");
+    // if (PROXY === undefined) {
+    //     console.error("pls set PROXY g")
+    //     process.exit(1)
+    // }
+    // return PROXY[~~(Math.random() * PROXY.length)]
+
+    let mullvadServers = await fetch("https://api.mullvad.net/www/relays/wireguard/").then(res => res.json()) as any[];
+    mullvadServers = mullvadServers.filter(s => s.active && s.country_code === "us")
+    const server = mullvadServers[~~(Math.random() * mullvadServers.length)];
+    return `socks5://${server.socks_name}:${server.socks_port}`
 }
 
-export const proxy = pickProxy();
+export const proxy = await pickProxy();
 
 console.log({ proxy });
 
