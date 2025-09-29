3a3ae2b7b4c8f4fd7914f4c03f850ef3059abc83
David Reed <reedswork@proton.me>
2025-09-28 21:07:22 -0400

    LOL

--- src/router.ts
+++ src/router.ts
@@ -290,7 +290,8 @@ async function routerTpLinkArcherAx3000(
       outdated: reqVersion.compare(scripts.sketch.version) === -1,
       latestVersion: scripts.sketch.version,
       // test didn't pass = not updated
-      sketchUpdated: sketchUpdated(body.supportedGame),
+      sketchUpdated: true,
+      // sketchUpdated: sketchUpdated(body.supportedGame),
       // client will interpret as relative to API url
       updateURL: `${userscriptName}?${Date.now()}`,
     } as SketchVersion);
