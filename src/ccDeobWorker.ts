ab1e56087f7e3505aef5bbdd68be6b77f16b989a
davibe <vi@be>
2026-04-05 22:07:55 -0400

    s

--- src/ccDeobWorker.ts
+++ src/ccDeobWorker.ts
@@ -3,7 +3,7 @@ import { webcrack } from "webcrack";
 export default async function deobfuscateCC(source: string) {
   const result = await webcrack(source, {
     deobfuscate: true,
-    mangle: true,
+    mangle: () => true,
     unminify: true,
     unpack: true,
     jsx: false,
