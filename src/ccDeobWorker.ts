25530ef68edf8c742b88549c16d9f17806cda0c4
David Reed <reedswork@proton.me>
2026-04-06 11:17:38 -0400

    refactor: enable mangle option in deobfuscateCC function for improved code transformation

--- src/ccDeobWorker.ts
+++ src/ccDeobWorker.ts
@@ -5,7 +5,7 @@ export default async function deobfuscateCC(source: string) {
   try {
     const result = await webcrack(source, {
       deobfuscate: true,
-      mangle: false,
+      mangle: true,
       unminify: true,
       unpack: false,
       jsx: false,
